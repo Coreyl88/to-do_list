a048b8bd4ed47f280cb7d5bc477d05a5b1191287
Corey Lott <Lott-c@hotmail.com>
2023-03-23 11:54:50 -0500

    finished basic todo list with mark complete. Now need to delete

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface item {
     id: number;
     text: string;
-    complete: boolean;
+    completed: boolean;
 }
 
 export const Todo: React.FC = () => {
+    
+    const [todos, setTodos] = useState<item[]>([
+        {id: 1, text: "Hello", completed: false},
+        {id: 2, text: "Help Me!", completed: false}
+    ])
+
+    const [input, setInput] = useState<string>("");
+
+    const handleToggle = (id: number) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            })
+        );
+    };
+
+    const handleSubmit = () => {
+        const newTodo: item = { id: Date.now(), text: input, completed: false };
+        setTodos([ ...todos, newTodo ]);
+    };
+
   return (
     <div className='main-container'>
         <h1>To-Do List</h1>
         <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
+            {todos.map((todo) =>(
+                <li key={todo.id} onClick={() => handleToggle(todo.id)} style={{textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</li>
+            ))}
         </ul>
         <input 
             type="text"
-            placeholder='Add Your To-do'/>
-        <button>Add</button>
+            placeholder='Add Your To-do' onChange={(e) => setInput(e.currentTarget.value)}/>
+        <button onClick={handleSubmit}>Add</button>
     </div>
   )
 }
